02609df3c0501a8c9829b5410208ca9f3e224fa2
Frank Bian <FrankBian@126.com>
2015-04-02 21:29:16 +0800

    code update

--- tomcat-code/target/classes/org/apache/catalina/startup/Bootstrap.java
+++ tomcat-code/target/classes/org/apache/catalina/startup/Bootstrap.java
@@ -16,6 +16,13 @@
  */
 package org.apache.catalina.startup;
 
+import org.apache.catalina.Globals;
+import org.apache.catalina.security.SecurityClassLoad;
+import org.apache.catalina.startup.ClassLoaderFactory.Repository;
+import org.apache.catalina.startup.ClassLoaderFactory.RepositoryType;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -26,13 +33,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import org.apache.catalina.Globals;
-import org.apache.catalina.security.SecurityClassLoad;
-import org.apache.catalina.startup.ClassLoaderFactory.Repository;
-import org.apache.catalina.startup.ClassLoaderFactory.RepositoryType;
-import org.apache.juli.logging.Log;
-import org.apache.juli.logging.LogFactory;
 
 
 /**
@@ -448,7 +448,8 @@ public final class Bootstrap {
      */
     public static void main(String args[]) {
 
-         System.out.println("Perfect ! It's started by Frank");
+        System.out.println("Perfect ! It's started by Frank");
+
         if (daemon == null) {
             // Don't set daemon until init() has completed
             Bootstrap bootstrap = new Bootstrap();
